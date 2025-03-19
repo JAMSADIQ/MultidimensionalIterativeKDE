89ef047919d09eb1df1163760297819bbe1df6c2
jxs1805 <jxs1805@rit.edu>
2025-03-19 17:23:08 +0100

    modify runscript.sh, eval 3d and utils plot to work for m1m2Xieff outputs

--- src/kde_analysis/eval_3dkde.py
+++ src/kde_analysis/eval_3dkde.py
@@ -1,24 +1,18 @@
 import sys
-import matplotlib
-#### set the path below where KDE code repo is
 sys.path.append('pop-de/popde/')
 import density_estimate as d
 import adaptive_kde as ad
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 import argparse
-import json
 import h5py as h5
-import scipy
-import numpy as np
 from scipy.integrate import quad, simpson
 from scipy.interpolate import RegularGridInterpolator
 from matplotlib import rcParams
 from matplotlib.colors import LogNorm, Normalize
-from astropy.cosmology import FlatLambdaCDM, z_at_value
-import astropy.units as u
 import utils_plot as u_plot
-import pandas as pd
+
 # Set Matplotlib parameters for consistent plotting
 rcParams.update({
     "text.usetex": True,
@@ -45,14 +39,15 @@ parser.add_argument('--datafilename2', help='h5  file containing N sample of par
 parser.add_argument('--datafilename3', help='h5  file containing N sample of dL for each event')
 parser.add_argument('--parameter1', help='name of parameter which we use for x-axis for KDE', default='m1')
 parser.add_argument('--parameter2', help='name of parameter which we use for y-axis for KDE: m2', default='m2')
-parser.add_argument('--parameter3', help='name of parameter which we use for y-axis for KDE [can be Xieff, dL]', default='dL')
+parser.add_argument('--parameter3', help='name of parameter which we use for y-axis for KDE [can be Xieff, dL]', default='Xieff')
 parser.add_argument('--m1-min', default=5.0, type=float, help='Minimum value for primary mass m1.')
-parser.add_argument('--m1-max', default=105.0, type=float, help='Maximum value for primary mass m1.')
-parser.add_argument('--Npoints', default=200, type=int, help='Number of points for KDE evaluation.')
+parser.add_argument('--m1-max', default=100.0, type=float, help='Maximum value for primary mass m1.')
+parser.add_argument('--Npoints-masses', default=150, type=int, help='Number of points for KDE evaluation.')
+parser.add_argument('--Npoints-param3', default=100, type=int, help='Number of points for KDE evaluation.')
 parser.add_argument('--param2-min', default=4.95, type=float, help='Minimum value for parameter 2 if it is  m2, else if dL use 10')
-parser.add_argument('--param2-max', default=105.0, type=float, help='Maximum value for parameter 2 if it is m2 else if dL  use 10000')
-parser.add_argument('--param3-min', default=200., type=float, help='Minimum value for parameter 3 if it is  dL, else if Xieff use -1')
-parser.add_argument('--param3-max', default=8000., type=float, help='Maximum value for parameter 3 if it is dL else if Xieff  use +1')
+parser.add_argument('--param2-max', default=100.0, type=float, help='Maximum value for parameter 2 if it is m2 else if dL  use 10000')
+parser.add_argument('--param3-min', default=-1., type=float, help='Minimum value for parameter 3 if it is  dL, use 500 else if Xieff use -1')
+parser.add_argument('--param3-max', default=1., type=float, help='Maximum value for parameter 3 if it is dL use 8000 else if Xieff  use +1')
 
 parser.add_argument('--discard', default=100, type=int, help=('discard first 100 iterations'))
 parser.add_argument('--NIterations', default=1000, type=int, help='Total number of iterations for the reweighting process.')
@@ -61,8 +56,58 @@ parser.add_argument('--NIterations', default=1000, type=int, help='Total number 
 #plots and saving data
 parser.add_argument('--pathplot', default='./', help='public_html path for plots', type=str)
 parser.add_argument('--iterative-result-filename', required=True, help='write a proper name of output hdf files based on analysis', type=str)
-parser.add_argument('--output-filename', default='m1_m2_chief_kde_eval_output-filename', help='write a proper name of output hdf files based on analysis', type=str)
+parser.add_argument('--VTdata-filename', required=True, help='VToncoarsegrid use for interpolator', type=str)
+parser.add_argument('--output-filename', default='m1m2mdL3Danalysis_slice_dLoutput-filename', help='write a proper name of output hdf files based on analysis', type=str)
 opts = parser.parse_args()
+
+#################Integration functions######################
+def integral_wrt_Xieff(KDE3D, VT3D, Xieff_grid, Nevents=69):
+    """
+    KDE3d and VT3d are computed with indexing ='ij' way
+    """
+    Rate3D = Nevents*KDE3D/VT3D
+    integm1m2 = simpson(Rate3D, x=Xieff_grid, axis=2)
+    return integm1m2
+
+
+def get_rate_m_oneD(m1_query, m2_query, Rate):
+    ratem1 = np.zeros(len(m1_query))
+    ratem2 = np.zeros(len(m2_query))
+    for xid, m1 in enumerate(m1_query):
+        y_valid = m2_query <= m1_query[xid]  # Only accept points with y <= x
+        rate_vals = Rate[y_valid, xid]
+        #print(rate_vals, m2_query[y_valid])
+        ratem1[xid] = simpson(rate_vals, m2_query[y_valid])
+    for yid, m2 in enumerate(m2_query):
+        x_valid = m1_query >= m2_query[yid]  # Only accept points with y <=
+        rate_vals = Rate[x_valid, yid]
+        ratem2[yid] = simpson(rate_vals,  m1_query[x_valid])
+    return ratem1, ratem2
+
+
+def get_rate_m_Xieff2D(m1_query, m2_query, Rate):
+    ratem1 = np.zeros((len(m1_query), Rate.shape[2]))
+    ratem2 = np.zeros((len(m2_query), Rate.shape[2]))
+
+    # Iterate over each slice along the third dimension
+    for i in range(Rate.shape[2]):
+        # Extract the 2D slice
+        Rate_slice = Rate[:, :, i]
+
+        # Compute ratem1
+        for xid, m1 in enumerate(m1_query):
+            y_valid = m2_query <= m1_query[xid]  # Only accept points with y <= x
+            rate_vals = Rate_slice[y_valid, xid]
+            ratem1[xid, i] = simpson(rate_vals, m2_query[y_valid])
+
+        # Compute ratem2
+        for yid, m2 in enumerate(m2_query):
+            x_valid = m1_query >= m2_query[yid]  # Only accept points with y <= x
+            rate_vals = Rate_slice[x_valid, yid]
+            ratem2[yid, i] = simpson(rate_vals, m1_query[x_valid])
+
+    return ratem1, ratem2
+
 #####################################################################
 f1 = h5.File(opts.datafilename1, 'r')#m1
 #f1 = h5.File('Final_noncosmo_GWTC3_m1srcdatafile.h5', 'r')#m1
@@ -82,54 +127,9 @@ f1.close()
 f2.close()
 f3.close()
 
-mean_m1 = np.array(medianlist1)
-mean_m2 = np.array(medianlist2)
-mean_chieff = np.array(medianlist3)
-
-################## To get KDE data on m1-Xieff or m2-Xieff wit integation over one of the mass
-def IntegrateRm1m2Xieff_wrt_m2(m1val, m2val, Xieffval, Ratem1m2Xieff):
-    # Initialize the output array (size: 150 x 100)
-    ratem1_Xieff = np.zeros((len(m1val), len(Xieffval)))
-    # Iterate over m1 indices
-    for xid, m1 in enumerate(m1val):
-        # Mask for valid m2 values where m2 <= m1
-        m2_valid = m2val <= m1  # Boolean mask
-        rate_vals = Ratem1m2_Xieff[xid, m2_valid, :]  # Shape: (valid_m2, 100)
-
-        # Perform Simpson integration along the m2 axis
-        if np.sum(m2_valid) > 1:  # Ensure there are at least two points for integration
-            ratem1_Xieff[xid, :] = simpson(rate_vals, x=m2val[m2_valid], axis=0)
-        else:
-            ratem1_Xieff[xid, :] = 0  # If no valid points, set to zero
-
-    return ratem1_Xieff
-
-##############2D m-Xieff plots
-def get_mXieff_plot(medianlist_m, medianlist_xieff, M, XIEFF, KDElist, iterN=1, mass_tag='m_1', pathplot='./', plot_name='KDE'):
-    data_slice = np.percentile(KDElist, 50, axis=0)
-    colorbar_label = r'$p('+mass_tag+', \chi_\mathrm{eff})$'
-    max_density = np.nanmax(data_slice)
-    max_exp = np.floor(np.log10(max_density))  # Find the highest power of 10 below max_density
-    contourlevels = 10 ** (max_exp - np.arange(4))[::-1]
-    contourlevels[-1] = max_density
-    print("contourlevels =", contourlevels)
-    plt.figure(figsize=(8, 6))
-    norm_val = LogNorm(vmin=contourlevels[0], vmax=max_density)  # Apply log normalization
-    pcm = plt.pcolormesh(M, XIEFF, data_slice, cmap='Purples', norm=norm_val, shading='auto')
-    contours = plt.contour(M,  XIEFF, data_slice, levels=contourlevels, colors='black')
-
-    # Colorbar
-    cbar = plt.colorbar(pcm, label=colorbar_label)
-    plt.scatter(medianlist_m, medianlist_xieff, color='r', marker='+', s=20)
-    plt.ylabel(r"$\chi_\mathrm{effective}$")
-    plt.xlabel(r"$m \,[M_\odot]$")
-    plt.semilogx()
-    plt.tight_layout()
-    plt.savefig(pathplot+"Average"+plot_name+mass_tag+"Xieff_Iter{0}.png".format(iterN))
-    #plt.close()
-    plt.show()
-    return 0
-
+meanxi1 = np.array(medianlist1)
+meanxi2 = np.array(medianlist2)
+meanxi3 = np.array(medianlist3)
 
 ####### to get KDEs we need eval points
 if opts.m1_min is not None and opts.m1_max is not None:
@@ -150,38 +150,60 @@ else:
 # can use opts but for now using fixed
 xmin, xmax = 5, 105  #m1
 ymin, ymax = 5, 105 #m2
-zmin, zmax = -1, 1 #dL
-Npoints = opts.Npoints
-chi_points = 100
+zmin, zmax = -1, 1 #Xieff
+Mpoints = opts.Npoints_masses #150
+Xipoints = opts.Npoints_param3 #100
 
+m1_src_grid = np.logspace(np.log10(5), np.log10(105), Mpoints)
+m2_src_grid = np.logspace(np.log10(5), np.log10(105), Mpoints)
+Xieff_grid = np.linspace(-1, 1, Xipoints)
 
-m1_src_grid = np.logspace(np.log10(5), np.log10(105), Npoints)
-m2_src_grid = np.logspace(np.log10(5), np.log10(105), Npoints)
-Xieff_grid = np.linspace(-1, 1, chi_points)
-#
-M, XIEFF = np.meshgrid(m1_src_grid, Xieff_grid, indexing='ij')
-hd2Dm2Xieff = h5.File(opts.output_filename+"integrate_m2_data_data.hdf5", "w")
-hd2Dm1Xieff = h5.File(opts.output_filename+"integrate_m1_data.hdf5", "w")
-#savehf = h5.File(opts.output_filename+"Full3D_data.hdf5", "w")
-
-hdf_file = opts.iterative_result_filename# Your HDF5 file path
+######## Iterative result files must have 'iteration_{i} format data see below
+hdf_file = opts.iterative_result_filename #
 hdf = h5.File(hdf_file, 'r')
-Total_Iterations = int(opts.NIterations)
-discard = int(opts.discard)
-
+###### KDE eval 3D grid #########################
 XX, YY, ZZ = np.meshgrid(m1_src_grid, m2_src_grid, Xieff_grid, indexing='ij')
 eval_samples = np.column_stack([XX.ravel(), YY.ravel(), ZZ.ravel()])
+######## For 2D plots ##############################
+M, XIEFF = np.meshgrid(m1_src_grid, Xieff_grid, indexing='ij')
+M1, M2 = np.meshgrid(m1_src_grid, m2_src_grid, indexing='ij')
+#################### VT data ###########################
+vt_file = opts.VTdata_filename #
+VTdata = h5.File(vt_file, 'r')
+m1vals = VTdata['m1'][...]
+m2vals = VTdata['m2'][...]
+Xieffvals = VTdata['Xieff'][...]
+VT_3D = VTdata['VT'][...]/1e9     #Gpc3^ need 1/1e9
+VTdata.close()
+#####In case Interpolation is needed not using can be issue ####################
+interp_VT = RegularGridInterpolator((m1_vals, m2_vals, Xieff_vals), VT_values, bounds_error=False, fill_value=np.nan) #nan can be an issue
+threeDgrid = np.array([XX.ravel(), YY.ravel(), ZZ.ravel()]).T
+#VT_3Dinterp = interp_VT(threeDgrid) #indexing ij can be issue here
 
-hd2Dm2Xieff.create_dataset("Mmesh", data=M)
-hd2Dm2Xieff.create_dataset("Xieffmesh", data=XIEFF)
+############ Saving data in 3 files #################################
+savehfintegm1m2 = h5.File(opts.output_filename+"CHI_PRIORsamples2Dm1m2_IntXieffdata1100Iterations.hdf5", "w")
+savehfintegm1Xieff = h5.File(opts.output_filename+"CHI_PRIORsamples2Dm1Xieff_Intdata1100Iterations.hdf5", "w")
+savehfintegm2Xieff = h5.File(opts.output_filename+"CHI_PRIORsamples2Dm1Xieff_m2Xieff_data1100Iterations.hdf5", "w")
 
-hd2Dm1Xieff.create_dataset("Mmesh", data=M)
-hd2Dm1Xieff.create_dataset("Xieffmesh", data=XIEFF)
+savehfintegm1m2.create_dataset("M1mesh", data=M1)
+savehfintegm1m2.create_dataset("M2mesh", data=M2)
+savehfintegm1Xieff.create_dataset("Mmesh", data=M)
+savehfintegm1Xieff.create_dataset("XIEFFmesh", data=XIEFF)
+savehfintegm2Xieff.create_dataset("Mmesh", data=M)
+savehfintegm2Xieff.create_dataset("XIEFFmesh", data=XIEFF)
 
+rate_m1m2IntXieff_list = []
+ratem1_arr= []#np.zeros(shape=[900, len(m1_src_grid)])
+ratem2_arr =[] #np.zeros(shape=[900, len(m2_src_grid)])
 
-#kde_list = []
-kde_m1chieff = []
-kde_m2chieff = []
+KDEM1Xieff = []
+KDEM2Xieff = []
+RateM1Xieff = []
+RateM2Xieff = []
+kde3d_list = [] #if needed
+###############################Iterations and evaluating KDEs/Rate
+Total_Iterations = int(opts.NIterations)
+discard = int(opts.discard)
 for i in range(Total_Iterations+discard): #fix this as this can change
         iteration_name = f'iteration_{i}'
         if iteration_name not in hdf:
@@ -212,38 +234,74 @@ for i in range(Total_Iterations+discard): #fix this as this can change
             )
         # Evaluate the KDE on the evaluation samples
         eval_kde3d = train_kde.evaluate_with_transf(eval_samples)
-        KDE_3d = eval_kde3d.reshape(XX.shape)
-        #kde_list.append(KDE_3d)
-        # do we want to save this?no
-        #savehf.create_dataset("kde_iter{0}".format(i), data=KDE_slice)
-        #savehf.flush()
-#savehf.close()
-        #integrate w r t m1
-        # Mask where m1 < m2 (set to zero)
-        mask_m1_lt_m2 = XX < YY
-        KDE3D_m1_integrate = np.where(mask_m1_lt_m2, 0, KDE_3d)
-        # Integrate over m1 where m1 >= m2
-        integrated_m1 = simpson(KDE3D_m1_integrate, x=m1_src_grid, axis=0)
-        hd2Dm2Xieff.create_dataset("kde_iter{0}".format(i), data=integrated_m1)
-        kde_m2chieff.append(integrated_m1)
+        KDE_slice = eval_kde3d.reshape(XX.shape)
+        Rate3D  = 69*KDE_slice/VT_3D
+        kdeM1Xieff, kdeM2Xieff = get_rate_m_Xieff2D(m1_src_grid, m2_src_grid, KDE_slice)
+        rateM1Xieff, rateM2Xieff = get_rate_m_Xieff2D(m1_src_grid, m2_src_grid, Rate3D)
+        KDEM1Xieff.append(kdeM1Xieff)
+        KDEM2Xieff.append(kdeM2Xieff)
+        RateM1Xieff.append(rateM1Xieff)
+        RateM2Xieff.append(rateM2Xieff)
+        Ratem1m2 = integral_wrt_Xieff(KDE_slice, VT_3D, Xieff_grid)
+        rate_m1m2IntXieff_list.append(Ratem1m2)
+        #I am not saving kde but rates only change it if u need kde
+        savehfintegm1m2.create_dataset("ratem1m2_iter{0}".format(i), data=Ratem1m2)
+        savehfintegm1Xieff.create_dataset("rate_m1xieff_iter{0}".format(i), data=rateM1Xieff)
+        savehfintegm2Xieff.create_dataset("rate_m2xieff_iter{0}".format(i), data=rateM2Xieff)
 
-        #integaret w r t m2
-        # Mask where m2 > m1 (set to zero)
-        mask_m2_gt_m1 = YY > XX
-        KDE3D_m2_integrate = np.where(mask_m2_gt_m1, 0, KDE_3d)
-        # Integrate over m2 where m2 <= m1
-        integrated_m2 = simpson(KDE3D_m2_integrate, x=m2_src_grid, axis=1)
-        hd2Dm1Xieff.create_dataset("kde_iter{0}".format(i), data=integrated_m2)
-        kde_m1chieff.append(integrated_m2)
-        if (i+1) % discard==0:
-            get_mXieff_plot(mean_m2, mean_chieff, M, XIEFF, kde_m2chieff[-discard:], iterN=i, mass_tag='m_1', pathplot=opts.pathplot, plot_name='KDE_Int_m1')
-            get_mXieff_plot(mean_m1, mean_chieff, M, XIEFF, kde_m1chieff[-discard:], iterN=i, mass_tag='m_2', pathplot=opts.pathplot, plot_name='KDE_Int_m2')
-        hd2Dm1Xieff.flush()
-        hd2Dm2Xieff.flush()
+        #get oneD output
+        rateM1, rateM2 = get_rate_m_oneD(m1_src_grid, m2_src_grid, Ratem1m2)
+        ratem1_arr.append(rateM1)
+        ratem2_arr.append(rateM2)
+        #if we need 3D output
+        #kde_list.append(KDE_slice)
+        savehfintegm2Xieff.create_dataset("rate_m1_iter{0}".format(i), data=rateM1)
+        savehfintegm2Xieff.create_dataset("rate_m2_iter{0}".format(i), data=raterateM2)
+        savehfintegm1m2.flush()
+        savehfintegm1Xieff.flush()
+        savehfintegm2Xieff.flush()
 
-hd2Dm1Xieff.close()
-hd2Dm2Xieff.close()
+savehfintegm1m2.close()
+savehfintegm1Xieff.close()
+savehfintegm2Xieff.close()
+#Plots only for final iterations if needed one can add this inside loop 
 
-get_mXieff_plot(mean_m2, mean_chieff, M, XIEFF, kde_m2chieff[discard:], iterN=1001, mass_tag='m_1', pathplot=opts.pathplot, plot_name='KDE_Int_m1')
-get_mXieff_plot(mean_m1, mean_chieff, M, XIEFF, kde_m1chieff[discard:], iterN=1001, mass_tag='m_2', pathplot=opts.pathplot, plot_name='KDE_Int_m2')
+#Note I did not save and make plot for m1-m2KDE
+u_plot.get_averagem1m2_plot(meanxi1, meanxi2, M1, M2, rate_m1m2IntXieff_list[100:], iterN=1, pathplot='./', plot_name='Rate')
 
+u_plot.get_m_Xieff_plot(meanxi1, meanxi3, M, XIEFF, KDEM1Xieff[discard:], iterN=1, pathplot='./', plot_name='KDE', xlabel = 'm_1')
+u_plot.get_m_Xieff_plot(meanxi2, meanxi3, M, XIEFF, KDEM2Xieff[discard:], iterN=1, pathplot='./', plot_name='KDE', xlabel = 'm_2')
+u_plot.get_m_Xieff_plot(meanxi1, meanxi3, M, XIEFF, RateM1Xieff[discard:], iterN=1, pathplot='./', plot_name='Rate', xlabel = 'm_1')
+u_plot.get_m_Xieff_plot(meanxi2, meanxi3, M, XIEFF, RateM2Xieff[discard:], iterN=1, pathplot='./', plot_name='Rate', xlabel = 'm_2')
+
+#One D plot and Offset will go here
+fig, ax = plt.subplots(figsize=(8, 4.5))
+# Define colors
+color_m1 = 'royalblue'
+color_m2 = 'darkorange'
+
+# Plot m1 data
+median_m1 = np.median(ratem1_arr, axis=0)
+p5_m1 = np.percentile(ratem1_arr, 5., axis=0)
+p95_m1 = np.percentile(ratem1_arr, 95., axis=0)
+ax.plot(m1_src_grid, median_m1, color=color_m1, linewidth=2, label='m1')
+ax.fill_between(m1_src_grid, p5_m1, p95_m1, color=color_m1, alpha=0.3)
+
+# Plot m2 data
+median_m2 = np.median(ratem2_arr, axis=0)
+p5_m2 = np.percentile(ratem2_arr, 5., axis=0)
+p95_m2 = np.percentile(ratem2_arr, 95., axis=0)
+ax.plot(m2_src_grid, median_m2, color=color_m2, linewidth=2, label='m2')
+ax.fill_between(m2_src_grid, p5_m2, p95_m2, color=color_m2, alpha=0.3)
+
+ax.set_xlim(4., 105.)
+ax.legend()
+ax.set_title(r'$\chi_\mathrm{eff} = 0$')
+ax.grid(True, ls="--")
+ax.set_ylabel(r'$\mathrm{d}\mathcal{R}/\mathrm{d}m \mathrm{d}\chi_\mathrm{eff}[\mathrm{Gpc}^{-3}\,\mathrm{yr}^{-1}\mathrm{M}_\odot^{-1}]$', fontsize=14)
+ax.set_xlabel(r"$m$", fontsize=20)
+plt.semilogy()
+plt.tight_layout()
+plt.show()
+
+######offset plot ######################
