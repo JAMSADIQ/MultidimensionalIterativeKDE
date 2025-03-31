e920d7c77d9f4e969711ec7ed0c594db8bc54d01
Thomas Dent <thomas.dent@ligo.org>
2025-03-31 04:37:03 -0700

    cleanup/simplify compute VT

--- src/kde_analysis/compute_VT.py
+++ src/kde_analysis/compute_VT.py
@@ -9,95 +9,94 @@
 # python compute_VT.py --m1points 150 --m2points 150 --Xipoints 100 --output Full3DVTm1m2Xieff.csv
 import os
 import csv
-import matplotlib.pyplot as plt
-import new_o123_class_found_inj_general as u_pdet
 import h5py as h5
 import argparse
 import numpy as np
-import pandas as pd
+from cbc_pdet import o123_class_found_inj_general as u_pdet
 
-parser = argparse.ArgumentParser(description="Generate VT values based on m1, m2, and Xieff.")
-parser.add_argument('--m1points', type=int, default=50, help='Number of points in m1 grid')
-parser.add_argument('--m2points', type=int, default=50, help='Number of points in m2 grid')
-parser.add_argument('--Xipoints', type=int, default=40, help='Number of points in Xieff grid')
-parser.add_argument('--fix_m1', type=float, help='Fix m1 at this value and slice along m2 and Xieff')
-parser.add_argument('--fix_m2', type=float, help='Fix m2 at this value and slice along m1 and Xieff')
-parser.add_argument('--fix_Xieff', type=float, help='Fix Xieff at this value and slice along m1 and m2')
+parser = argparse.ArgumentParser(description="Generate VT values based on m1, m2, and chi_eff.")
+parser.add_argument('--m1points', type=int, default=150, help='Number of points in m1 grid')
+parser.add_argument('--m2points', type=int, default=150, help='Number of points in m2 grid')
+parser.add_argument('--chieffpoints', type=int, default=100, help='Number of points in z grid')
+parser.add_argument('--min-m1', type=float, default=3.)
+parser.add_argument('--max-m1', type=float, default=110.)
+parser.add_argument('--min-m2', type=float, default=3.)
+parser.add_argument('--max-m2', type=float, default=110.)
+parser.add_argument('--min-chieff', type=float, default=-0.99)
+parser.add_argument('--max-chieff', type=float, default=0.99)
+parser.add_argument('--fix_m1', type=float, help='Fix m1 at this value and slice along m2 and chieff')
+parser.add_argument('--fix_m2', type=float, help='Fix m2 at this value and slice along m1 and chieff')
+parser.add_argument('--fix_chieff', type=float, help='Fix chieff at this value and slice along m1 and m2')
 parser.add_argument('--output', type=str, required=True, help='Output file name')
 args = parser.parse_args()
 
 ########### for VT computation #################################
-run_fit = 'o3'
-run_dataset = 'o3'
-dmid_fun = 'Dmid_mchirp_fdmid_fspin' #'Dmid_mchirp_fdmid'
+dmid_fun = 'Dmid_mchirp_fdmid_fspin'
 emax_fun = 'emax_exp'
 alpha_vary = None
-g = u_pdet.Found_injections(dmid_fun = 'Dmid_mchirp_fdmid_fspin', emax_fun='emax_exp', alpha_vary = None, ini_files = None, thr_far = 1, thr_snr = 10)
-g.load_inj_set(run_dataset)
-g.get_opt_params(run_fit)
-g.set_shape_params()
+pdet = u_pdet.Found_injections(dmid_fun=dmid_fun, emax_fun=emax_fun, alpha_vary=None, ini_files=None, thr_far=1, thr_snr=10)
+pdet.load_inj_set('o3')
+pdet.get_opt_params('o3')
+pdet.set_shape_params()
 
 #############Grids ###############
-m1 = np.logspace(np.log10(5), np.log10(105), args.m1points)
-m2 = np.logspace(np.log10(5), np.log10(105), args.m2points)
-Xieff = np.linspace(-1, 1, args.Xipoints)
-
+m1 = np.logspace(np.log10(args.min_m1), np.log10(args.max_m1), args.m1points)
+m2 = np.logspace(np.log10(args.min_m2), np.log10(args.max_m2), args.m2points)
+cf = np.linspace(args.min_chieff, args.max_chieff, args.chieffpoints)
 
 csv_file = args.output
 if not os.path.exists(csv_file):
     with open(csv_file, mode='w', newline='') as f:
         writer = csv.writer(f)
-        writer.writerow(['m1', 'm2', 'Xieff', 'VT'])  # Write header row
+        writer.writerow(['m1', 'm2', 'chieff', 'VT'])  # Write header row
 
-
-# Initialize VT array
 if args.fix_m1 is not None:
-    # Fix m1 and for loop along m2 and Xieff
+    # Fix m1 and for loop along m2 and chieff
     fixed_m1 = args.fix_m1
     # Open the CSV file in append mode
     with open(csv_file, mode='a', newline='') as f:
         writer = csv.writer(f)
-        for j, m2val in enumerate(m2):
-            for k, Xival in enumerate(Xieff):
-                VT = g.total_sensitive_volume(fixed_m1, m2val, chieff =Xival)
-                writer.writerow([fixed_m1, m2val, Xival, VT])
+        for m2val in m2:
+            for cfv in cf:
+                VT = pdet.total_sensitive_volume(fixed_m1, m2val, chieff=cfv)
+                writer.writerow([fixed_m1, m2val, cfv, VT])
 
 elif args.fix_m2 is not None:
-    # Fix m2 and loop along m1 and Xieff
+    # Fix m2 and loop along m1 and chieff
     fixed_m2 = args.fix_m2
-    # Open the CSV file in append mode
     with open(csv_file, mode='a', newline='') as f:
         writer = csv.writer(f)
-        for i, m1val in enumerate(m1):
-            for k, Xival in enumerate(Xieff):
-                VT =   g.total_sensitive_volume(m1val, fixed_m2, chieff =Xival) 
-                writer.writerow([m1val, fixed_m2, Xival, VT])
+        for m1val in m1:
+            for cfv in cf:
+                VT = pdet.total_sensitive_volume(m1val, fixed_m2, chieff=cfv)
+                writer.writerow([m1val, fixed_m2, cfv, VT])
 
-elif args.fix_Xieff is not None:
-    # Fix Xieff and slice along m1 and m2
-    fixed_Xieff = args.fix_Xieff
-    # Open the CSV file in append mode
+elif args.fix_chieff is not None:
+    # Fix chieff and slice along m1 and m2
+    fixed_chieff = args.fix_chieff
     with open(csv_file, mode='a', newline='') as f:
         writer = csv.writer(f)
-        for i, m1val in enumerate(m1):
-            for j, m2val in enumerate(m2):
-                VT =  g.total_sensitive_volume(m1val, m2val, chieff =fixed_Xieff)
-                writer.writerow([m1val, m2val, fixed_Xieff, VT])
+        for m1val in m1:
+            for m2val in m2:
+                VT = pdet.total_sensitive_volume(m1val, m2val, chieff=fixed_chieff)
+                writer.writerow([m1val, m2val, fixed_chieff, VT])
 
 else:
-    # Open the CSV file in append mode
     with open(csv_file, mode='a', newline='') as f:
         writer = csv.writer(f)
-    # No fixed variable, compute full 3D VT
-        for i, m1val in enumerate(m1):
-            for j, m2val in enumerate(m2):
-                for k, Xival in enumerate(Xieff):
-                    VT = g.total_sensitive_volume(m1val, m2val, chieff =Xival)
-                    writer.writerow([m1val, m2val, Xival, VT])
+        # No fixed variable, compute full 3D VT
+        for m1val in m1:
+            for m2val in m2:
+                print(m1val, m2val)
+                for cfv in cf:
+                    VT = pdet.total_sensitive_volume(m1val, m2val, chieff=cfv)
+                    writer.writerow([m1val, m2val, cfv, VT])
 
 quit()
+
 #read file and interplote get VT plot 
 import pandas as pd
+import matplotlib.pyplot as plt
 df = pd.read_csv(csv_file)
 m1_vals = np.sort(df['m1'].unique())
 m2_vals = np.sort(df['m2'].unique())
