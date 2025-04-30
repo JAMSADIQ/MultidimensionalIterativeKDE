74e4baf7e7405b017b97810e1bf2582dfb15c775
Thomas Dent <thomas.dent@ligo.org>
2025-04-30 08:15:20 -0700

    .sh script changes to avoid repeated PE samples

--- data/processed/select_nocosmo_samples_chieff_prior.sh
+++ data/processed/select_nocosmo_samples_chieff_prior.sh
@@ -4,4 +4,4 @@ python ../../src/data_scripts/get_random_PE_nocosmo.py \
   --rsample 100 \
   --inverse-chieff-prior-weight \
   --o2filesname ${JAMDATADIR}/GWTC1/*.h5  --o3afilesname ${JAMDATADIR}/GWTC2_1/NONcosmo/*.h5 --o3bfilesname ${JAMDATADIR}/GWTC3/NONcosmo/*.h5 \
-  --tag GWTC3_NOCOSMO_TEST_CHIEFF_PRIOR --seed 314 --eventsType BBH --min-median-mass 5 
+  --tag GWTC3_NOCOSMO_12345_CFPRIOR_NOREP --seed 12345 --eventsType BBH --min-median-mass 3
--- src/kde_analysis/runscript_3Dm1m2Xieffcase.sh
+++ src/kde_analysis/runscript_3Dm1m2Xieffcase.sh
@@ -1,1 +1,11 @@
-nohup python spin_m1m2case.py --reweight-sample-option reweight --min-bw3 0.1 --parameter1 m1 --parameter2 m2  --parameter3 chieff --samples-dim1 ../../data/processed/GWTC3_NOCOSMO_314_CHIEFF_PRIOR_m1src.h5 --samples-dim2 ../../data/processed/GWTC3_NOCOSMO_314_CHIEFF_PRIOR_m2src.h5 --samples-dim3 ../../data/processed/GWTC3_NOCOSMO_314_CHIEFF_PRIOR_chieff.h5 --pathplot m1m2chieff_250404/ --output-filename m1m2chieff_250404/900it --n-iterations 900 --buffer-start 200  --buffer-interval 100 --samples-redshift ../../data/processed/GWTC3_NOCOSMO_314_CHIEFF_PRIOR_redshift.h5 --samples-dl ../../data/processed/GWTC3_NOCOSMO_314_CHIEFF_PRIOR_dl.h5 --samples-vt VT_314_CHIEFF_PRIOR.hdf5 & 
+SAMPLES=NOCOSMO_12345_CFPRIOR_NOREP
+
+nohup python spin_m1m2case.py --reweight-sample-option reweight --min-bw3 0.1 --parameter1 m1 --parameter2 m2 --parameter3 chieff \
+  --samples-dim1 ../../data/processed/GWTC3_${SAMPLES}_m1src.h5 \
+  --samples-dim2 ../../data/processed/GWTC3_${SAMPLES}_m2src.h5 \
+  --samples-dim3 ../../data/processed/GWTC3_${SAMPLES}_chieff.h5 \
+  --pathplot m1m2chieff_250430/ --output-filename m1m2chieff_250430/12345_norep \
+  --n-iterations 900 --buffer-start 200 --buffer-interval 100 \
+  --samples-redshift ../../data/processed/GWTC3_${SAMPLES}_redshift.h5 \
+  --samples-dl ../../data/processed/GWTC3_${SAMPLES}_dl.h5 \
+  --samples-vt m1m2chieff_250430/VT_12345_CFPRIOR_NOREP.hdf5 & 
